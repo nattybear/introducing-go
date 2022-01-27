844362b1f2b12061f7244f462b7091b287ea3015
준규 <vpark45@gmail.com>
2022-01-27 23:29:30 +0900

    An Example Program

--- src/chapter3/main.go
+++ src/chapter3/main.go
@@ -3,6 +3,11 @@ package main
 import "fmt"
 
 func main() {
-  const x string = "Hello, World"
-  fmt.Println(x)
+  fmt.Print("Enter a number: ")
+  var input float64
+  fmt.Scanf("%f", &input)
+
+  output := input * 2
+
+  fmt.Println(output)
 }
