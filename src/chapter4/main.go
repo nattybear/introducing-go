a8cda12f85a4dcedaf09683e88bd4caeeba7dbb8
준규 <vpark45@gmail.com>
2022-01-29 21:45:21 +0900

    The switch Statement

--- src/chapter4/main.go
+++ src/chapter4/main.go
@@ -4,10 +4,14 @@ import "fmt"
 
 func main() {
   for i := 1; i <= 10; i++ {
-    if i % 2 == 0 {
-      fmt.Println(i, "even")
-    } else {
-      fmt.Println(i, "odd")
+    switch i {
+    case 0: fmt.Println("Zero")
+    case 1: fmt.Println("One")
+    case 2: fmt.Println("Two")
+    case 3: fmt.Println("Three")
+    case 4: fmt.Println("Four")
+    case 5: fmt.Println("Five")
+    default: fmt.Println("Unknown Number")
     }
   }
 }
