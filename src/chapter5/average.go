ab11002780496765f13deb7b3977ea9c17d934c4
준규 <vpark45@gmail.com>
2022-01-31 21:44:30 +0900

    The Go compiler won't allow you to create variables that you never use

--- src/chapter5/average.go
+++ src/chapter5/average.go
@@ -11,8 +11,8 @@ func main() {
   x[4] = 83
 
   var total float64 = 0
-  for i := 0; i < len(x); i++ {
-    total += x[i]
+  for i, value := range x {
+    total += value
   }
   fmt.Println(total / float64(len(x)))
 }
