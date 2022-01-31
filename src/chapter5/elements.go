ca5451ed64a2844b5f152cb947712e35170ca93e
준규 <vpark45@gmail.com>
2022-01-31 22:38:46 +0900

    Shorter way to create maps

--- src/chapter5/elements.go
+++ src/chapter5/elements.go
@@ -3,17 +3,18 @@ package main
 import "fmt"
 
 func main() {
-  elements := make(map[string]string)
-  elements["H"]  = "Hydrogen"
-  elements["He"] = "Helium"
-  elements["Li"] = "Lithium"
-  elements["Be"] = "Beryllium"
-  elements["B"]  = "Boron"
-  elements["C"]  = "Carbon"
-  elements["N"]  = "Nitogen"
-  elements["O"]  = "Oxygen"
-  elements["F"]  = "Fluorine"
-  elements["Ne"] = "Neon"
+  elements := map[string]string{
+    "H"  : "Hydrogen",
+    "He" : "Helium",
+    "Li" : "Lithium",
+    "Be" : "Beryllium",
+    "B"  : "Boron",
+    "C"  : "Carbon",
+    "N"  : "Nitogen",
+    "O"  : "Oxygen",
+    "F"  : "Fluorine",
+    "Ne" : "Neon",
+  }
 
   if name, ok := elements["Un"]; ok {
     fmt.Println(name, ok)
