2b418c99c275500a3bfcb2c3a49c9617ae243fe7
준규 <vpark45@gmail.com>
2022-02-01 22:52:06 +0900

    Create average.go

---
+++ src/chapter6/average.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func average(xs []float64) float64 {
+  panic("Not Implemented")
+}
+
+func main() {
+  xs := []float64{98,93,77,82,83}
+  fmt.Println(average(xs))
+}
