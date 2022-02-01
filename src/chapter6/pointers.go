d815551881c0f9442dce0fcf7c2790d77c7d162d
준규 <vpark45@gmail.com>
2022-02-02 08:34:07 +0900

    Use pointer

--- src/chapter6/pointers.go
+++ src/chapter6/pointers.go
@@ -2,12 +2,12 @@ package main
 
 import "fmt"
 
-func zero(x int) {
-  x = 0
+func zero(xPtr *int) {
+  *xPtr = 0
 }
 
 func main() {
   x := 5
-  zero(x)
-  fmt.Println(x)  // x is still 5
+  zero(&x)
+  fmt.Println(x)  // x is 0
 }
