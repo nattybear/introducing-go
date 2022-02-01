5b0cc6bc05d5487394a477f98aaf7b813ac51797
준규 <vpark45@gmail.com>
2022-02-02 08:14:47 +0900

    Pair it with defer

--- src/chapter6/panic-and-recover.go
+++ src/chapter6/panic-and-recover.go
@@ -3,7 +3,9 @@ package main
 import "fmt"
 
 func main() {
+  defer func() {
+    str := recover()
+    fmt.Println(str)
+  }()
   panic("PANIC")
-  str := recover()  // this will never happen
-  fmt.Println(str)
 }
