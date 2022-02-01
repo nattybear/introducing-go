afeef11ea3b7be35b59b0670dd8ee145cfa4b65b
준규 <vpark45@gmail.com>
2022-02-01 22:53:03 +0900

    Implement average function body

--- src/chapter6/average.go
+++ src/chapter6/average.go
@@ -3,7 +3,11 @@ package main
 import "fmt"
 
 func average(xs []float64) float64 {
-  panic("Not Implemented")
+  total := 0.0
+  for _, v := range xs {
+    total += v
+  }
+  return total / float64(len(xs))
 }
 
 func main() {
