0910a522935bd47a80b7a73861acf1316355e9ad
준규 <vpark45@gmail.com>
2022-02-11 00:34:00 +0900

    Rewrite totalArea using interface

--- src/chapter7/shape.go
+++ src/chapter7/shape.go
@@ -11,7 +11,7 @@ type Circle struct {
 }
 
 type Shape interface {
-  arae() float64
+  area() float64
 }
 
 func distance(x1, y1, x2, y2 float64) float64 {
@@ -30,25 +30,16 @@ func (c *Circle) area() float64 {
   return math.Pi * c.r*c.r
 }
 
-func totalArea(circles []Circle, rectangles []Rectangle) float64 {
-  var total float64
-  for _, c := range circles {
-    total += c.area()
+func totalArea(shapes ...Shape) float64 {
+  var area float64
+  for _, s := range shapes {
+    area += s.area()
   }
-  for _, r := range rectangles {
-    total += r.area()
-  }
-  return total
+  return area
 }
 
 func main() {
-  circles := []Circle{
-    Circle{0, 0, 5},
-    Circle{0, 0, 6},
-  }
-  rectangles := []Rectangle{
-    Rectangle{1,1,2,2},
-    Rectangle{2,2,3,3},
-  }
-  fmt.Println(totalArea(circles, rectangles))
+  c := Circle{0, 0, 5}
+  r := Rectangle{1,1,2,2}
+  fmt.Println(totalArea(&c, &r))
 }
