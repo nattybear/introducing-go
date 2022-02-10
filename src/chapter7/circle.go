ec4a690e596cb363a3754f64a725f68331830ba3
준규 <vpark45@gmail.com>
2022-02-10 23:58:32 +0900

    Rewrite using method

--- src/chapter7/circle.go
+++ src/chapter7/circle.go
@@ -6,11 +6,11 @@ type Circle struct {
   x, y, r float64
 }
 
-func circleArea(c *Circle) float64 {
+func (c *Circle) area() float64 {
   return math.Pi * c.r*c.r
 }
 
 func main() {
   c := Circle{0, 0, 5}
-  fmt.Println(circleArea(&c))
+  fmt.Println(c.area())
 }
