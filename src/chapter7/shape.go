3ca0e292bef0c17a3ede030d8c2d11d0265dc203
준규 <vpark45@gmail.com>
2022-02-11 00:01:47 +0900

    Add Rectangle struct

--- src/chapter7/shape.go
+++ src/chapter7/shape.go
@@ -2,15 +2,19 @@ package main
 
 import ("fmt"; "math")
 
+type Rectangle struct {
+  x1, y1, x2, y2 float64
+}
+
 func distance(x1, y1, x2, y2 float64) float64 {
   a := x2 - x1
   b := y2 - y1
   return math.Sqrt(a*a + b*b)
 }
 
-func rectangleArea(x1, y1, x2, y2 float64) float64 {
-  l := distance(x1, y1, x1, y2)
-  w := distance(x1, y1, x2, y1)
+func (r *Rectangle) area() float64 {
+  l := distance(r.x1, r.y1, r.x1, r.y2)
+  w := distance(r.x1, r.y1, r.x2, r.y1)
   return l * w
 }
 
@@ -19,10 +23,6 @@ func circleArea(x, y, r float64) float64 {
 }
 
 func main() {
-  var rx1, ry1 float64 = 0, 0
-  var rx2, ry2 float64 = 10, 10
-  var cx, cy, cr float64 = 0, 0, 5
-
-  fmt.Println(rectangleArea(rx1, ry1, rx2, ry2))
-  fmt.Println(circleArea(cx, cy, cr))
+  r := Rectangle{0, 0, 10, 10}
+  fmt.Println(r.area())
 }
