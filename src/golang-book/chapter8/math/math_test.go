951356a7451407abac3d9469e5412fca09c6934a
준규 <vpark45@gmail.com>
2022-02-28 21:18:55 +0900

    Exercise 8-2

--- src/golang-book/chapter8/math/math_test.go
+++ src/golang-book/chapter8/math/math_test.go
@@ -4,23 +4,22 @@ import "testing"
 
 type testpair struct {
   values []float64
-  average float64
+  min float64
 }
 
 var tests = []testpair{
-    { []float64{1,2}, 1.5 },
+    { []float64{1,2}, 1 },
     { []float64{1,1,1,1,1,1}, 1 },
-    { []float64{-1,1}, 0 },
-    { []float64{}, 0},
+    { []float64{-1,1}, -1 },
 }
 
 func TestAverage(t *testing.T) {
   for _, pair := range tests {
-    v := Average(pair.values)
-    if v != pair.average {
+    v := Min(pair.values)
+    if v != pair.min {
       t.Error(
         "For", pair.values,
-        "expected", pair.average,
+        "expected", pair.min,
         "got", v,
       )
     }
