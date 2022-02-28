74d87c848216ee0b6c109e222e3976f56a7b2592
준규 <vpark45@gmail.com>
2022-02-28 20:59:38 +0900

    Exercise 4

--- src/golang-book/chapter8/main.go
+++ src/golang-book/chapter8/main.go
@@ -6,5 +6,9 @@ import "golang-book/chapter8/math"
 func main() {
   xs := []float64{1,2,3,4}
   avg := math.Average(xs)
-  fmt.Println(avg);
+  fmt.Println(avg)
+  min := math.Min(xs)
+  fmt.Println(min)
+  max := math.Max(xs)
+  fmt.Println(max)
 }
--- src/golang-book/chapter8/math/math.go
+++ src/golang-book/chapter8/math/math.go
@@ -7,3 +7,23 @@ func Average(xs []float64) float64 {
   }
   return total / float64(len(xs))
 }
+
+func Min(xs []float64) float64 {
+  min := xs[0]
+  for _, x := range xs {
+    if x < min {
+      min = x
+    }
+  }
+  return min
+}
+
+func Max(xs []float64) float64 {
+  max := xs[0]
+  for _, x := range xs {
+    if x > max {
+      max = x
+    }
+  }
+  return max
+}
